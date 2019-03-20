dcd18df6dd7c39e21b5291223f92c927cdc720ae
Darryn <darryn.patterson@gmail.com>
2019-03-20 16:32:58 +0200

    Implemented proof of work algorithm (hashing difficulty).

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const { DIFFICULTY } = require("../config");
 
 describe("Block", () => {
   let data, lastBlock, block;
@@ -15,5 +16,10 @@ describe("Block", () => {
 
   test("sets the `lastHash` to match the hash of the last batch", () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  test("generates a hash that matches the difficulty", () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual("0".repeat(DIFFICULTY));
+    console.log(block.toString());
   });
 });
